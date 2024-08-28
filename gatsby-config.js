c28e41f0d7d793f18d5572539121caec53bfab0e
Adiela Abishua <adiela.abishua@gmail.com>
2024-08-28 18:35:09 +0300

    Customize gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,15 +9,15 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Adiela Abishua - Software Engineering Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Adiela Abishua`,
+      summary: `Software Engineer with years of experience B2B and B2C software in multiple industries`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
+    description: `Software Engineering Blog`,
+    siteUrl: `https://adiela.me/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `adielaabishua`,
     },
   },
   plugins: [
@@ -102,7 +102,7 @@ module.exports = {
               }
             }`,
             output: "/rss.xml",
-            title: "Gatsby Starter Blog RSS Feed",
+            title: "Adiela Abishua - Software Engineering Blog RSS Feed",
           },
         ],
       },
@@ -110,15 +110,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `Gatsby`,
+        name: `Adiela Abishua - Software Engineering Blog`,
+        short_name: `Adiela Abishua`,
         start_url: `/`,
         background_color: `#ffffff`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo.svg`, // This path is relative to the root of the site.
       },
     },
   ],
