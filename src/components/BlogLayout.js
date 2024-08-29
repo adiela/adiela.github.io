7565143bee44f4b404305220ca31e0cc234b7d29
Adiela Abishua <adiela.abishua@gmail.com>
2024-08-29 12:31:15 +0300

    Blog Layout component edits

--- src/components/BlogLayout.js
+++ src/components/BlogLayout.js
@@ -1,36 +1,21 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import Layout from "../components/Layout"
 
-const Layout = ({ location, title, children }) => {
-  const rootPath = `${__PATH_PREFIX__}/`
-  const isRootPath = location.pathname === rootPath
-  let header
-
-  if (isRootPath) {
-    header = (
-      <h1 className="main-heading">
-        <Link to="/">{title}</Link>
-      </h1>
-    )
-  } else {
-    header = (
-      <Link className="header-link-home" to="/">
-        {title}
-      </Link>
-    )
-  }
+const BlogLayout = ({ children }) => {
 
   return (
-    <div className="blog-wrapper" data-is-root-path={isRootPath}>
-      <header className="global-header">{header}</header>
-      <main>{children}</main>
-      <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
-      </footer>
-    </div>
+    <Layout>
+      <div className="blog-wrapper">
+        <main>{children}</main>
+        <footer>
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.com">Gatsby</a>
+        </footer>
+      </div>
+    </Layout>
+    
   )
 }
 
-export default Layout
+export default BlogLayout
