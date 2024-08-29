2e07b014f62c3fdfc84b38e1744cd3511ca8a8ba
Adiela Abishua <adiela.abishua@gmail.com>
2024-08-29 12:30:19 +0300

    Update 404 and blog pages to use 'BlogLayout' layout component

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,17 +1,17 @@
 import * as React from "react"
 import { graphql } from "gatsby"
 
-import Layout from "../components/BlogLayout"
+import BlogLayout from "../components/BlogLayout"
 import Seo from "../components/seo"
 
 const NotFoundPage = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
 
   return (
-    <Layout location={location} title={siteTitle}>
+    <BlogLayout location={location} title={siteTitle}>
       <h1>404: Not Found</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </Layout>
+    </BlogLayout>
   )
 }
 
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,7 +2,7 @@ import * as React from "react"
 import { Link, graphql } from "gatsby"
 
 import Bio from "../components/bio"
-import Layout from "../components/BlogLayout"
+import BlogLayout from "../components/BlogLayout"
 import Seo from "../components/seo"
 
 const BlogIndex = ({ data, location }) => {
@@ -11,19 +11,19 @@ const BlogIndex = ({ data, location }) => {
 
   if (posts.length === 0) {
     return (
-      <Layout location={location} title={siteTitle}>
+      <BlogLayout location={location} title={siteTitle}>
         <Bio />
         <p>
           No blog posts found. Add markdown posts to "content/blog" (or the
           directory you specified for the "gatsby-source-filesystem" plugin in
           gatsby-config.js).
         </p>
-      </Layout>
+      </BlogLayout>
     )
   }
 
   return (
-    <Layout location={location} title={siteTitle}>
+    <BlogLayout location={location} title={siteTitle}>
       <Bio />
       <ol style={{ listStyle: `none` }}>
         {posts.map(post => {
@@ -57,7 +57,7 @@ const BlogIndex = ({ data, location }) => {
           )
         })}
       </ol>
-    </Layout>
+    </BlogLayout>
   )
 }
 
