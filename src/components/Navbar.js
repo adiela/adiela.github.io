1a1fa6c3df980dba7783b27f2f921ff25b8eae16
Adiela Abishua <adiela.abishua@gmail.com>
2024-08-30 14:12:48 +0300

    Edit navbar and navbar styling for responsiveness

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import { Link } from "gatsby"
 
 const navLinks = [
     {text: "About", address: "/"},
-    {text: "Projects", address: "/#projects"},
+    {text: "Projects", address: "#"},
     {text: "Blog", address: "/blog"},
     {text: "Contact", address: "/contact"},
 ]
@@ -15,8 +15,14 @@ const Navbar = () => {
     <nav id="navbar">
         <Link to="/"><img src={logo} alt="Logo"/></Link>
 
-        <ul>
-        {navLinks.map(link => <li key={link.text}><Link to={link.address}>{link.text}</Link></li>)}
+        <input id="menu-toggle" type="checkbox"/>
+        <label htmlFor="menu-toggle" id="menu-toggle-label" className="hamburger-lines">
+          <span class="line line1"></span>
+          <span class="line line2"></span>
+          <span class="line line3"></span>
+        </label>
+        <ul id="nav-list"> 
+          {navLinks.map(link => <li key={link.text}><Link to={link.address} className="nav-link">{link.text}</Link></li>)}
         </ul>
         
     </nav>
