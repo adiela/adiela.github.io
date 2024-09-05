0ed079df9df9ae36e3ce2b98d0abd8140a4ad4f4
Adiela Abishua <adiela.abishua@gmail.com>
2024-09-05 15:53:57 +0300

    Contact page: make contact form functional

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,104 @@
-import * as React from "react"
+import React, { useState } from 'react';
 import SocialLinks from "../components/SocialLinks"
 import Layout from "../components/Layout"
+
+const ContactForm = () => {
+
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        message: '',
+      });
+
+    const [errors, setErrors] = useState({});  
+
+    const validate = () => {
+        const errors = {};
+    
+        if (!formData.name.trim()) {
+          errors.name = 'Name is required';
+        }
+    
+        const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        if (!formData.email) {
+          errors.email = 'Email is required';
+        } else if (!emailPattern.test(formData.email)) {
+          errors.email = 'Invalid email address';
+        }
+    
+        if (!formData.message.trim()) {
+          errors.message = 'Message is required';
+        }
+    
+        return errors;
+      };
+
+      const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setFormData({
+          ...formData,
+          [name]: value,
+        });
+      }; 
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const validationErrors = validate();
+    setErrors(validationErrors);
+
+    if (Object.keys(validationErrors).length === 0) {
+        // Form submission logic goes here
+        fetch("https://formspree.io/f/xrgojjly", {
+            method: "POST",
+            body: JSON.stringify(formData),
+            headers: {
+              "Content-type": "application/json; charset=UTF-8"
+            }
+          });
+
+        // Reset form after successful submission
+        setFormData({
+          name: '',
+          email: '',
+          message: '',
+        });
+      }
+    }
+
+  return (
+    <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          id="comtact-name"
+          name="name"
+          value={formData.name}
+          onChange={handleInputChange}
+          placeholder="Name"
+        />
+        {errors.name && <p className='form-error' style={{ color: 'red' }}>{errors.name}</p>}
+        <input
+          type="email"
+          id="email"
+          name="contact-email"
+          value={formData.email}
+          onChange={handleInputChange}
+          placeholder="Email"
+        />
+        {errors.email && <p className='form-error' style={{ color: 'red' }}>{errors.email}</p>}
+        <textarea
+          id="message"
+          name="contact-message"
+          value={formData.message}
+          onChange={handleInputChange}
+          placeholder='Message'
+          rows="10"
+        />
+        {errors.message && <p className="form-error" style={{ color: 'red' }}>{errors.message}</p>}
+      <button type="submit" className="button">Submit</button>
+    </form>
+  );
+};
 
 
 const ContactPage = () => {
@@ -14,12 +112,7 @@ const ContactPage = () => {
                 </p>
                 <SocialLinks/>
             </header>
-            <form>
-                <input type="text" name="name" placeholder="Enter your name" />
-                <input type="text" name="email" placeholder="Enter your email" />
-                <textarea type="text" name="message" placeholder="Type your message here" rows="10" />
-                <input type="button" value="Send" />
-            </form>
+            <ContactForm/>
         </main>
     </Layout>
   )
@@ -27,4 +120,4 @@ const ContactPage = () => {
 
 export default ContactPage
 
-export const Head = () => <title>Contact Page</title>
+export const Head = () => <title>Contact | Adiela Abishua</title>
