2fa3d59092cd698373c50c0b648b30d9fe1a8c08
Adiela Abishua <adiela.abishua@gmail.com>
2024-09-11 19:19:12 +0300

    Update bio text

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function IndexPage() {
             <p className="heading-3">Hi there, I'm</p>
             <h1 className="heading-1 font-xl">Adiela Abishua</h1>
             <h2 className="heading-2">Full-Stack Software Engineer</h2>
-            <p id="bio">I am a software engineer with over 5 years of professional experience. I have built B2C and  B2B software. I am passionate about the use of technology in solving real world problems.</p>
+            <p id="bio">I am a software engineer with over 5 years of professional experience in building both B2C and B2B software. I am passionate about the use of technology in solving real world problems. I build software that is performant, and user friendly.</p>
             <a href="/resume.pdf" className="button float-right">View Resume</a>
           </div>
         </section>
