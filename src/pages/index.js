303d7ded1b24590b0f74402162222465f82481b0
Adiela Abishua <adiela.abishua@gmail.com>
2024-09-24 11:24:57 +0300

    Update index page styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
 import { StaticImage } from 'gatsby-plugin-image';
 import SocialLinks from '../components/SocialLinks';
 import Layout from '../components/Layout';
@@ -14,9 +13,9 @@ function IndexPage() {
             <SocialLinks />
           </div>
           <div className="column2">
-            <p className="heading-3">Hi there, I'm</p>
+            <span className="greeting">Hi there, I'm</span>
             <h1 className="heading-1 font-xl">Adiela Abishua</h1>
-            <h2 className="heading-2">Full-Stack Software Engineer</h2>
+            <h2 className="sub-heading">Full-Stack Software Engineer</h2>
             <p id="bio">I am a software engineer with over 5 years of professional experience in building both B2C and B2B software. I am passionate about the use of technology in solving real world problems. I build software that is performant, and user friendly.</p>
             <a href="/resume.pdf" className="button float-right">View Resume</a>
           </div>
